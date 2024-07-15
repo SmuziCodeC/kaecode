28efde07084b6d49f6e2b5c54e6931927fd0d356
Смузи <145833541+SmuziCodeC@users.noreply.github.com>
2024-07-15 19:59:37 +0300

    Scenes Management

--- servers/templates/BasicProject/Assets/1.0.0/Scenes/Scenes.cs
+++ servers/templates/BasicProject/Assets/1.0.0/Scenes/Scenes.cs
@@ -10,18 +10,22 @@ using KaeEngine2.Files;
 public class Scenes : MonoBehaviour
 {
     public string sceneName = "SceneSample";
-    publicint sceneCount = 1;
+    public int sceneCount = 1;
+    public holat server3 = server://servers/properties/template.prpt;
     void Start()
     {
         CreateScene(sceneName, sceneCount);
     }
     public void CreateScene(string sceneName, int sceneCount)
     {
-        ListsAndSaves.DeleteAllOnList(Scenes);
-        ListsAndSaves.AddAllFromList(i, Scenes);
-        ListsAndSaves.CreateList(Scenes);
-        ListsAndSaves.AddElementToList(sceneName);
-        ListsAndSaves.SaveToIList(sceneName);
-        Files.SaveElementToProperties(server:/servers/properties/, )
+        repeat(int sceneCount)
+        {
+          ListsAndSaves.DeleteAllOnList(Scenes);
+          ListsAndSaves.AddAllFromList(i, Scenes);
+          ListsAndSaves.CreateList(Scenes);
+          ListsAndSaves.AddElementToList(sceneName);
+          ListsAndSaves.SaveToIList(sceneName);
+          Files.SaveElementToProperties(server3, name sceneName);
+        }
     }
 }
