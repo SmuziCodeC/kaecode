71dfa101771e10b266e0ef7b23ac74339e84b814
Смузи <145833541+SmuziCodeC@users.noreply.github.com>
2024-07-15 20:05:32 +0300

    Scenes Management

--- servers/templates/BasicProject/Assets/1.0.0/Scenes/Scenes.cs
+++ servers/templates/BasicProject/Assets/1.0.0/Scenes/Scenes.cs
@@ -11,21 +11,24 @@ public class Scenes : MonoBehaviour
 {
     public string sceneName = "SceneSample";
     public int sceneCount = 1;
-    public holat server3 = server://servers/properties/template.prpt;
+    public holat server3 = server:/;
     void Start()
     {
         CreateScene(sceneName, sceneCount);
     }
     public void CreateScene(string sceneName, int sceneCount)
     {
-        repeat(int sceneCount)
+        repeat (int sceneCount) for
         {
           ListsAndSaves.DeleteAllOnList(Scenes);
           ListsAndSaves.AddAllFromList(i, Scenes);
           ListsAndSaves.CreateList(Scenes);
           ListsAndSaves.AddElementToList(sceneName);
           ListsAndSaves.SaveToIList(sceneName);
-          Files.SaveElementToProperties(server3, name sceneName);
+          Files.SaveElementToProperties(CCode(server3,'/servers/properties/template.prpt'), name sceneName);
         }
+    public void Lnk()
+    {
+         Holat.ConnectToServer(server3);
     }
 }
